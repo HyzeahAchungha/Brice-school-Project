fe852cc160aa672ba5292c86422ba6344dc89df5
clintjeff2 <clintjeff2@gmail.com>
2024-07-16 03:56:33 +0100

    Made the student login and ignup  pages with backend routes to handle functionality

--- Backend/Routes/AdminRoutes.js
+++ Backend/Routes/AdminRoutes.js
@@ -1,28 +1,31 @@
 import express from 'express';
-import con from "../utils/db.js"
-import jwt from 'jsonwebtoken'
-
+import con from '../utils/db.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-
 // router.get('/adminlogin')
 router.post('/adminlogin', (req, res) => {
-   const sql = "SELECT * FROM admin where email=? and password=?"
-   con.query(sql, [req.body.email, req.body.password], (err, result) => {
-      if (err) return res.json({ loginStatus: false, Error: "Querry failed" })
-      if (result.length > 0) {
-         const email = result[0].email
-         const token = jwt.sign({ role: "admin", email: email }, 'jwt_secret_key', { expiresIn: '1d' })
+	const sql = 'SELECT * FROM admin where email=? and password=?';
+	con.query(sql, [req.body.email, req.body.password], (err, result) => {
+		if (err) return res.json({ loginStatus: false, Error: 'Querry failed' });
+		if (result.length > 0) {
+			const email = result[0].email;
+			const token = jwt.sign(
+				{ role: 'admin', email: email },
+				'jwt_secret_key',
+				{ expiresIn: '1d' }
+			);
 
-         res.cookie('token', token)
-      return res.json({ loginStatus: true })
-      }else{
-         return res.json({ loginStatus: false, Error: "Wrong email or password " })
-      }
-      
-   });
+			res.cookie('token', token);
+			return res.json({ loginStatus: true });
+		} else {
+			return res.json({
+				loginStatus: false,
+				Error: 'Wrong email or password ',
+			});
+		}
+	});
+});
 
-})
-
-export { router as adminRouter } 
+export { router as adminRouter };
--- Frontend/brice-project/src/App.js
+++ Frontend/brice-project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './component/login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './component/Dashboard';
@@ -10,30 +10,29 @@ import Category from './component/Category';
 import Students from './component/Students';
 import Chats from './component/Chats';
 import Profile from './component/Profile';
+import StudentLogin from './component/students/Login';
+import StudentSignup from './component/students/Signup';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/adminlogin" element={<Login />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="" element={<Home />}></Route>
-          <Route path="students" element={<Students />}></Route>
-          <Route path="chat" element={<Chats />}></Route>
-          <Route path="profile" element={<Profile />}></Route>
-          <Route path="newsweek" element={<NewsWeek />}></Route>
-          <Route path="category" element={<Category />}></Route>
-          </Route>
-
-        </Routes>
-
-      </BrowserRouter>
-
-
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/adminlogin" element={<Login />}></Route>
+					<Route path="/dashboard" element={<Dashboard />}>
+						<Route path="" element={<Home />}></Route>
+						<Route path="students" element={<Students />}></Route>
+						<Route path="chat" element={<Chats />}></Route>
+						<Route path="profile" element={<Profile />}></Route>
+						<Route path="newsweek" element={<NewsWeek />}></Route>
+						<Route path="category" element={<Category />}></Route>
+					</Route>
+					<Route path="/students/login" element={<StudentLogin />} />
+					<Route path="/students/signup" element={<StudentSignup />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
